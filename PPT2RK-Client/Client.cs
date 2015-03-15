e335b7ed74d69f5ea5d87c7a1eaa57f4c3afdc0d
Anders Gjermshus <anders.gjermshus@onestopreporting.com>
2015-03-15 03:43:41 +0100

    Initial commit

---
+++ PPT2RK-Client/Client.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PPT2RK_Client
+{
+    public class Client
+    {
+        private PolarPersonalTrainerLib.PPTExport _polarexporter;
+        private RKClient _rkclient;
+
+        public Client(string PolarUsername, string PolarPassword, string RK_ClientId, string RK_ClientSecret)
+        {
+            try
+            {
+                this.Initiate(PolarUsername, PolarPassword, RK_ClientId, RK_ClientSecret);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Client()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            try
+            {
+                this.Initiate(appSettings["PPTUsername"], appSettings["PPTPassword"], appSettings["RKClientId"], appSettings["RKClientSecret"], appSettings["RKAccessToken"]);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void Initiate(string PolarUsername, string PolarPassword, string RK_ClientId, string RK_ClientSecret, string RK_accessToken = null)
+        {
+            try
+            {
+                this._polarexporter = new PolarPersonalTrainerLib.PPTExport(PolarUsername, PolarPassword);
+                this._rkclient = new RKClient(RK_ClientId, RK_ClientSecret, RK_accessToken);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Gets todays activities
+        /// </summary>
+        /// <returns>Number of excercises transfered</returns>
+        public Task<int> TransferTodaysAcitivities()
+        {
+            DateTime startTime = DateTime.Today;
+            DateTime endTime = DateTime.Today.AddDays(1).AddSeconds(-1);
+            // Get todays activities from PPT
+            XmlDocument doc = this._polarexporter.downloadSessions(startTime, endTime);
+            List<PolarPersonalTrainerLib.PPTExercise> excercises = PolarPersonalTrainerLib.PPTExtract.convertXmlToExercises(doc);
+            int count = 0;
+            foreach (var PPTExercise in excercises)
+            {
+                if (!this._rkclient.ExcerciseExists(PPTExercise))
+                {
+                    this._rkclient.AddExercise(PPTExercise);
+                    count++;
+                }
+            }
+
+            return Task.FromResult(count);
+        }
+        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Number of excercises transfered</returns>
+        public Task<int> TransferAcitivities(DateTime day)
+        {
+            DateTime startTime= new DateTime(day.Year, day.Month, day.Day, 0,0,0);
+            DateTime endTime = startTime.AddDays(1).AddSeconds(-1);
+            // Get todays activities from PPT
+            XmlDocument doc = this._polarexporter.downloadSessions(startTime, endTime);
+            List<PolarPersonalTrainerLib.PPTExercise> excercises = PolarPersonalTrainerLib.PPTExtract.convertXmlToExercises(doc);
+            int count = 0;
+            foreach (var PPTExercise in excercises)
+            {
+                if (!this._rkclient.ExcerciseExists(PPTExercise))
+                {
+                    this._rkclient.AddExercise(PPTExercise);
+                    count++;
+                }
+            }
+
+            return Task.FromResult(count);
+        }
+
+        public Task<int> TransferAcitivities(DateTime from, DateTime To)
+        {
+            DateTime startTime = from;
+            DateTime endTime = To;
+            // Get todays activities from PPT
+            XmlDocument doc = this._polarexporter.downloadSessions(startTime, endTime);
+            List<PolarPersonalTrainerLib.PPTExercise> excercises = PolarPersonalTrainerLib.PPTExtract.convertXmlToExercises(doc);
+            int count = 0;
+            foreach (var PPTExercise in excercises)
+            {
+                if (!this._rkclient.ExcerciseExists(PPTExercise))
+                {
+                    this._rkclient.AddExercise(PPTExercise);
+                    count++;
+                }
+            }
+
+            return Task.FromResult(count);
+        }
+    }
+}
---
+++ PPT2RK-Client/RKClient.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthGraphNet;
+using HealthGraphNet.Models;
+using System.Configuration;
+
+namespace PPT2RK_Client
+{
+    public class RKClient
+    {
+        private string _clientId;
+        private string _clientSecret;
+        private string _accessToken;
+        private AccessTokenManager _tm;
+        private HealthGraphNet.Models.UsersModel _user;
+
+        public bool IsConnected { get; private set; }
+
+        public RKClient(string clientId, string clientSecret, string accessToken = null)
+        {
+            this._clientId = clientId;
+            this._clientSecret = clientSecret;
+            this._accessToken = accessToken;
+            this.IsConnected = false;
+        }
+
+        public Task<bool> Connect()
+        {
+            try
+            {
+                this._tm = new AccessTokenManager(this._clientId, this._clientSecret, "https://runkeeper.com/apps/authorize", this._accessToken);
+                
+                var userRequest = new UsersEndpoint(this._tm);
+                this._user = userRequest.GetUser();
+                this.IsConnected = true;
+                return Task.FromResult<bool>(true);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult<bool>(false);
+            }
+        }
+
+        internal void AddExercise(PolarPersonalTrainerLib.PPTExercise PPTExercise)
+        {
+            if (this.IsConnected == false)
+                this.Connect();
+
+            HeartRateModel hrm = new HeartRateModel();
+            var newAcitivty = new FitnessActivitiesNewModel()
+            {
+                Type = PPTExercise.sport ?? "Running",
+                StartTime = PPTExercise.time,
+                Duration = PPTExercise.duration.TotalSeconds,
+                TotalCalories = PPTExercise.calories,
+                HeartRate = PPTExercise.heartRate.HeartBeats.Select(e => { 
+                    return new HeartRateModel() 
+                    { 
+                        HeartRate = e.HeartRate, 
+                        Timestamp = (e.Time - PPTExercise.time).TotalSeconds 
+                    };
+                }).ToList(),
+                AverageHeartRate = PPTExercise.heartRate.average,
+                TotalDistance = PPTExercise.Distance
+            };
+
+            var activitiesRequest = new FitnessActivitiesEndpoint(this._tm, this._user);
+            activitiesRequest.CreateActivity(newAcitivty);
+        }
+
+        internal bool ExcerciseExists(PolarPersonalTrainerLib.PPTExercise PPTExercise)
+        {
+            if (this.IsConnected == false)
+                this.Connect();
+
+            var activitiesRequest = new FitnessActivitiesEndpoint(this._tm, this._user);
+            var acitivities = activitiesRequest.GetFeedPage(noEarlierThan: PPTExercise.time, noLaterThan: PPTExercise.time);
+            return acitivities.Items.Count > 0;
+        }
+    }
+}
